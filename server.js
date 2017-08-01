783b21c7be6765eba21bebd9a93d946c1f6afe34
kominam <kominam.2511@gmail.com>
2017-08-01 14:01:50 +0700

    Initialize model

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const express = require("express")
-const chalk = require("chalk")
-const app = express()
+require('dotenv').config()
 
-app.listen(8080, function() {
-  console.log("App's running at port 8080\n")
-  console.log("-> Press CTRL-C to stop\n")
+const express = require('express')
+const chalk = require('chalk')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.set('port', process.env.PORT || 8080)
+
+app.listen(app.get('port'), function() {
+  console.log('%s App is running at port %d in %s env\n',
+    chalk.green('✓'),
+    app.get('port'),
+    app.get('env'))
+  console.log('-> Press CTRL-C to stop\n')
+})
+
+app.get('/', (req, res) => {
+  res.send('It\'s ok now')
 })
